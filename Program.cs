76c129b7c267be7f575f730ff0a019894617c2c5
WiestL <wiestl@etsu.edu>
2024-11-04 19:54:19 -0500

    Hosting the website on azure setup

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using ProjectPortfolio.Contexts;
 using Microsoft.Extensions.DependencyInjection;
 using ProjectPortfolio.Models;
 using System.Text.Json.Serialization;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,38 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Azure App Configuration to the container.
+var azAppConfigConnection = builder.Configuration["AppConfig"];
+if (!string.IsNullOrEmpty(azAppConfigConnection))
+{
+    // Use the connection string if it is available.
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(azAppConfigConnection)
+        .ConfigureRefresh(refresh =>
+        {
+            // All configuration values will be refreshed if the sentinel key changes.
+            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+        });
+    });
+}
+else if (Uri.TryCreate(builder.Configuration["Endpoints:AppConfig"], UriKind.Absolute, out var endpoint))
+{
+    // Use Azure Active Directory authentication.
+    // The identity of this app should be assigned 'App Configuration Data Reader' or 'App Configuration Data Owner' role in App Configuration.
+    // For more information, please visit https://aka.ms/vs/azure-app-configuration/concept-enable-rbac
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(endpoint, new VisualStudioCredential())
+        .ConfigureRefresh(refresh =>
+        {
+            // All configuration values will be refreshed if the sentinel key changes.
+            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+        });
+    });
+}
+builder.Services.AddAzureAppConfiguration();
 builder.Services.AddRazorPages();
 
 builder.Services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve; });
@@ -60,6 +93,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseAzureAppConfiguration();
 
 app.UseRouting();
 
