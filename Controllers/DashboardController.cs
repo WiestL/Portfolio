6abee75608dab47d5d70aa5eae0ad06dbe4b117c
WiestL <wiestl@etsu.edu>
2024-12-04 13:13:34 -0500

    Adding admin dashboard to see stats

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjectPortfolio.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectPortfolio.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Dashboard()
+        {
+            ViewData["ProjectCount"] = await _context.Projects.CountAsync();
+            ViewData["ReviewCount"] = await _context.Testimonials.CountAsync();
+            ViewData["SkillCount"] = await _context.Skills.CountAsync();
+            ViewData["CategoryCount"] = await _context.Categories.CountAsync();
+
+            return View();
+        }
+    }
+}
