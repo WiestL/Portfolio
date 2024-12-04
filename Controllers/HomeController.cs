01fb890ddc126d6da906333d4f80cc4dc64e5916
WiestL <wiestl@etsu.edu>
2024-12-04 13:23:20 -0500

    Fixing stats dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,25 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjectPortfolio.Contexts;
 using ProjectPortfolio.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ProjectPortfolio.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
-
 
         public IActionResult Portfolio()
         {
             return View();
         }
-
-
 
         public IActionResult Index()
         {
@@ -29,6 +31,17 @@ namespace ProjectPortfolio.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AdminDashboard()
+        {
+            ViewData["ProjectCount"] = await _context.Projects.CountAsync();
+            ViewData["ReviewCount"] = await _context.Testimonials.CountAsync();
+            ViewData["SkillCount"] = await _context.Skills.CountAsync();
+            ViewData["CategoryCount"] = await _context.Categories.CountAsync();
+
+            return View(); // Maps to Views/Home/AdminDashboard.cshtml
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
